505fb4e6c56ac5a5531e46272177053cbe220eba
david <davmwa90@gmail.com>
2023-06-01 13:27:24 +0300

    updated agency component

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -6,11 +6,11 @@ import {MdLocationOn, MdOutlineExplore} from 'react-icons/md'
 const Destinations = () => {
   return (
     <div name="destinations" className='w-full h-full'>
-        <div className='flex flex-col w-full h-full max-w-[1240px] mx-auto justify-center items-center my-32'>
+        <div className='flex flex-col w-full h-full max-w-[1240px] mx-auto justify-center items-center lg:my-16'>
             <div>
               <h1 className='font-[Montserrat] font-thin uppercase border-b border-gray-400 pb-4 text-2xl' >Around the World</h1>
             </div>
-            <div className='mt-16 flex flex-col text-center'>
+            <div className=' mt-6 lg:mt-16 flex flex-col text-center'>
               <h2 className='text-7xl'>It's time to <span className='text-[var(--primary)]'>travel</span></h2>
               <p className='text-2xl py-6 leading-10'>Take a look at the most exclusive & most visited locations in the world - hand-picked just for you. Start traveling the world today!</p>
             </div>
